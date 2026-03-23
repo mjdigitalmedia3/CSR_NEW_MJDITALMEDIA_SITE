d9154d0591078f5b7e29acaf0d21b4e3c32c95b4
mjdigitalmedia3 <mjdigitalmedia3@users.noreply.github.com>
2026-03-22 22:17:57 -0400

    style: reduce spacing between CEO photo and bio text

--- client/src/pages/about-ceo.tsx
+++ client/src/pages/about-ceo.tsx
@@ -41,7 +41,7 @@ export default function AboutCEO() {
       </section>
 
       {/* Bio Section */}
-      <section className="py-8 lg:py-12">
+      <section className="py-4 lg:py-6">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="space-y-12">
             
